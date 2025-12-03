b5261b33bb10c33709c3144334689e20eed3bdab
Amanda Alves <foiassimumsonho@gmail.com>
2025-12-03 20:19:29 -0300

    Andamento do projeto

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -11,31 +10,16 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  // Controle do menu lateral
-  menuOpen: boolean = false;
+  email = '';
+  senha = '';
+  menuOpen = false;
+
+  slides = ['1.png', '2.png', '3.png'];
+  currentIndex = 0;
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
   }
-
-  // Login
-  email: string = '';
-  senha: string = '';
-
-  constructor(private router: Router) {}
-
-  entrar() {
-    if (this.email === 'admin@admin.com' && this.senha === '1234') {
-      alert('Login realizado com sucesso!');
-      this.router.navigate(['/dashboard']); // ou qualquer rota que exista
-    } else {
-      alert('E-mail ou senha incorretos!');
-    }
-  }
-
-  // Carrossel
-  slides = ['1.png', '2.png', '3.png'];
-  currentIndex = 0;
 
   nextSlide() {
     this.currentIndex = (this.currentIndex + 1) % this.slides.length;
@@ -43,5 +27,13 @@ export class HomeComponent {
 
   prevSlide() {
     this.currentIndex = (this.currentIndex - 1 + this.slides.length) % this.slides.length;
+  }
+
+  entrar() {
+    if (this.email === 'admin@admin.com' && this.senha === '1234') {
+      alert('Login realizado com sucesso!');
+    } else {
+      alert('E-mail ou senha incorretos!');
+    }
   }
 }
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,10 +1,11 @@
-// src/app/routes.ts
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' }, // "full" é obrigatório aqui
-  { path: 'home', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
-  { path: 'sobre', loadComponent: () => import('./pages/sobre/sobre.component').then(m => m.SobreComponent) },
-  { path: 'fale', loadComponent: () => import('./pages/fale/fale.component').then(m => m.FaleComponent) },
-  { path: '**', redirectTo: 'home', pathMatch: 'full' } // se quiser wildcard
+  // redireciona '' para home
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+
+  // rotas principais
+  { path: 'home', loadComponent: () => import('./home/home.component').then(m => m.HomeComponent) },
+  { path: 'sobre', loadComponent: () => import('./sobre/sobre.component').then(m => m.SobreComponent) },
+  { path: 'fale', loadComponent: () => import('./fale/fale.component').then(m => m.FaleComponent) }
 ];
